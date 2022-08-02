2484d9c7183dc347764d72efcd53eb0e5de6cbe8
vantheman2210 <martinovic.ba@gmail.com>
2022-08-02 19:46:34 +0200

    incorporated  firestore

---
+++ index.js
@@ -0,0 +1,133 @@
+let submit = document.getElementById('submit');
+let form = document.getElementById('bookForm');
+
+submit.addEventListener('click', (event) => {
+	let title = document.getElementById('titleForm').value;
+	let pages = document.getElementById('pagesForm').value;
+	let author = document.getElementById('authorForm').value;
+
+	if (author.trim() === '' || title.trim() === '') { 
+		return false;
+	} else if (isNaN(pages)) {
+		alert('Pages must be numerical'); 
+    return false;
+	} else { 
+    addBookToLibrary();
+		form.reset();
+  }
+});
+
+readButton.addEventListener('click', () => {
+	if (readButton.innerText === 'READ') {
+		readButton.innerText = 'NOT READ';
+	} else {
+		readButton.innerText = 'READ';
+	}
+});
+
+let myLibrary = [];
+
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = Number(pages);
+		this.read = read;
+	}
+}
+
+function addBookToLibrary() {
+	let titleNew = document.getElementById('titleForm').value;
+	let authorNew = document.getElementById('authorForm').value;
+	let pagesNew = document.getElementById('pagesForm').value;
+	let readNew = document.getElementById('readButton').innerText;
+
+	let book = new Book(titleNew, authorNew, pagesNew, readNew);
+	myLibrary.push(book);
+	displayLibrary();
+}
+
+function readStatus(number, string) {
+	myLibrary[number]['read'] = string;
+}
+
+const populateStorage = () => {
+	localStorage.setItem('library', JSON.stringify(myLibrary));
+};
+
+const getStorage = () => {
+	myLibrary = JSON.parse(localStorage.getItem('library'));
+};
+
+function displayLibrary() {
+	populateStorage();
+
+	let container = document.getElementById('container');
+	container.innerHTML = '';
+
+	for (i = 0; i < myLibrary.length; i++) {
+		let index = myLibrary[i];
+
+		let grid = document.createElement('div');
+		grid.id = i;
+		grid.setAttribute('value', i);
+		grid.setAttribute('class', 'books');
+
+		let author = document.createElement('h2');
+		author.id = 'author';
+
+		let title = document.createElement('h3');
+		title.id = 'title';
+
+		let pages = document.createElement('h3');
+		pages.id = 'pages';
+
+		let read = document.createElement('button');
+		read.id = 'read';
+
+		let remove = document.createElement('button');
+		remove.id = 'remove';
+
+		document.getElementById('container').appendChild(grid);
+		grid.appendChild(author);
+		grid.appendChild(title);
+		grid.appendChild(pages);
+		grid.appendChild(read);
+		grid.appendChild(remove);
+
+		author.innerHTML = 'Author: ' + myLibrary[i].author;
+		title.innerHTML = 'Title: ' + myLibrary[i].title;
+		pages.innerHTML = 'Pages: ' + myLibrary[i].pages;
+		read.innerText = myLibrary[i].read;
+		remove.innerHTML = '<i class="fa fa-trash fa-2x"></i>';
+
+		remove.addEventListener('click', () => {
+			index = grid.id;
+			myLibrary.splice(index, 1);
+			displayLibrary();
+		});
+
+		read.addEventListener('click', () => {
+			index = grid.id;
+
+			if (read.innerText == 'READ') {
+				read.innerText = 'NOT READ';
+				readStatus(index, 'NOT READ');
+				populateStorage();
+			} else if (read.innerText == 'NOT READ') {
+				read.innerText = 'READ';
+				readStatus(index, 'READ');
+				populateStorage();
+			}
+		});
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	if (!localStorage.getItem('library')) {
+		populateStorage();
+	} else {
+		getStorage();
+		displayLibrary();
+	}
+});
--- script.js
+++ script.js
@@ -1,127 +1,245 @@
-let submit = document.getElementById("submit");  
-let form = document.getElementById("bookForm");
-  submit.addEventListener("click", () => {    
-    addBookToLibrary();  
-    form.reset();
-  }) 
+let submit = document.getElementById('submit');
+let form = document.getElementById('bookForm');
+const container = document.getElementById('container');
+submit.addEventListener('click', () => {
+	addBookToLibrary();
+	form.reset();
+});
 
-  readButton.addEventListener("click", () => { 
-    if(readButton.innerText === "READ") { 
-      readButton.innerText = "NOT READ";
-    } else { 
-      readButton.innerText = "READ";
-    }
-  })
+readButton.addEventListener('click', () => {
+	if (read) {
+		readButton.innerText = 'NOT READ';
+	} else {
+		readButton.innerText = 'READ';
+	}
+});
 
-let myLibrary = []; 
+let myLibrary = [];
 
-class Book { 
-  constructor (title, author, pages, read) {
-  this.title = title; 
-  this.author = author; 
-  this.pages = Number(pages); 
-  this.read = read;  
-  } 
-  
-}  
+class Book {
+	constructor(title, author, pages, read = false) {
+		this.title = title;
+		this.author = author;
+		this.pages = Number(pages);
+		this.read = read;
+	}
+}
 
-function addBookToLibrary() {   
-  
-  let titleNew = document.getElementById("titleForm").value; 
-  let authorNew = document.getElementById("authorForm").value; 
-  let pagesNew = document.getElementById("pagesForm").value;  
-  let readNew = document.getElementById("readButton").innerText; 
-  
-  let book = new Book(titleNew, authorNew, pagesNew, readNew);  
-    myLibrary.push(book); 
-    displayLibrary();
-}  
+function addBookToLibrary() {
+	let titleNew = document.getElementById('titleForm').value;
+	let authorNew = document.getElementById('authorForm').value;
+	let pagesNew = document.getElementById('pagesForm').value;
+	let readNew = document.getElementById('readButton').innerText;
 
-function readStatus(number, string) {  
-  myLibrary[number]['read'] = string; 
+	let book = new Book(titleNew, authorNew, pagesNew, readNew);
+
+	if (auth.currentUser) {
+		console.log(auth.currentUser);
+		addBookDb(book);
+	} else {
+		myLibrary.push(book);
+	}
+	displayLibrary();
+}
+
+function readStatus(number, condition) {
+	console.log(number, condition);
+	console.log(myLibrary[number].read);
+	myLibrary[number]['read'] = condition === true ? false : true;
 }
 
 const populateStorage = () => {
-  localStorage.setItem('library', JSON.stringify(myLibrary));
-}
+	localStorage.setItem('library', JSON.stringify(myLibrary));
+};
 
 const getStorage = () => {
-  myLibrary = JSON.parse(localStorage.getItem('library'));
+	myLibrary = JSON.parse(localStorage.getItem('library'));
+};
+
+function displayLibrary() {
+	container.innerHTML = '';
+	if (auth.currentUser === null) {
+		console.log('null');
+		populateStorage();
+	}
+
+	for (i = 0; i < myLibrary.length; i++) {
+		let index = myLibrary[i];
+
+		let grid = document.createElement('div');
+		grid.id = i;
+		grid.setAttribute('value', i);
+		grid.setAttribute('class', 'books');
+
+		let author = document.createElement('h2');
+		author.id = 'author';
+
+		let title = document.createElement('h3');
+		title.id = 'title';
+
+		let pages = document.createElement('h3');
+		pages.id = 'pages';
+
+		let read = document.createElement('button');
+		read.id = 'read';
+
+		let remove = document.createElement('button');
+		remove.id = 'remove';
+
+		document.getElementById('container').appendChild(grid);
+		grid.appendChild(author);
+		grid.appendChild(title);
+		grid.appendChild(pages);
+		grid.appendChild(read);
+		grid.appendChild(remove);
+
+		author.innerHTML = 'Author: ' + myLibrary[i].author;
+		title.innerHTML = 'Title: ' + myLibrary[i].title;
+		pages.innerHTML = 'Pages: ' + myLibrary[i].pages;
+		read.innerText = myLibrary[i].read;
+		remove.innerHTML = '<i class="fa fa-trash fa-2x"></i>';
+
+		remove.addEventListener('click', () => {
+			index = grid.id;
+			const firestoreTitle = myLibrary[index].title;
+			myLibrary.splice(index, 1);
+			if (auth.currentUser !== null) {
+				console.log('user');
+				deleteBookDb(firestoreTitle);
+			}
+			displayLibrary();
+			console.log('click');
+		});
+
+		read.addEventListener('click', () => {
+			const index = grid.id;
+			const isRead = myLibrary[0].read;
+
+			if (isRead) {
+				read.innerText = 'NOT READ';
+				readStatus(index, true);
+				populateStorage();
+			} else {
+				read.innerText = 'READ';
+				readStatus(index, false);
+				populateStorage();
+			}
+		});
+	}
 }
 
-function displayLibrary () {    
+// Auth
+const auth = firebase.auth();
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
 
-  populateStorage(); 
+auth.onAuthStateChanged((user) => {
+	container.innerHTML = '';
+	if (user) {
+		db.collection('books').onSnapshot((snapshot) => {
+			console.log(snapshot.docs);
+			myLibrary = docsToBooks(snapshot.docs);
+			displayLibrary();
+		});
+	} else {
+		console.log('anonymous');
+		storage();
+		displayLibrary();
+	}
+});
 
-  let container = document.getElementById("container"); 
-  container.innerHTML = "";
+const signIn = () => {
+	console.log('----signin----');
+	container.innerHTML = '';
+	const provider = new firebase.auth.GoogleAuthProvider();
+	firebase
+		.auth()
+		.setPersistence(firebase.auth.Auth.Persistence.NONE)
+		.then(() => {
+			return provider;
+		})
+		.catch((error) => {
+			// Handle Errors here.
+			console.log(error);
+		});
+	auth.signInWithPopup(provider);
+	signInBtn.style.display = 'none';
+	signOutBtn.style.display = 'block';
+};
 
-  for(i = 0; i < myLibrary.length; i++) {    
+const signOut = () => {
+	console.log('-------singout-------');
+	console.log('logout');
+	container.innerHTML = '';
+	myLibrary = [];
+	auth.signOut();
+	signInBtn.style.display = 'block';
+	signOutBtn.style.display = 'none';
+	storage();
+	displayLibrary();
+};
 
-    let index = myLibrary[i];   
-    
-   
-    let grid = document.createElement("div");  
-    grid.id =  i;   
-    grid.setAttribute("value", i); 
-    grid.setAttribute("class", "books");
-    
-    let author = document.createElement("h2");  
-    author.id = "author"; 
+signInBtn.addEventListener('click', signIn);
+signOutBtn.addEventListener('click', signOut);
 
-    let title = document.createElement("h3");  
-    title.id = "title"; 
+// Local storage
+const storage = () => {
+	container.innerHTML = '';
+	if (!auth.currentUser === null) {
+		console.log('user logged');
+		return;
+	} else {
+		console.log('here');
 
-    let pages = document.createElement("h3");  
-    pages.id = "pages"; 
+		if (!localStorage.getItem('library')) {
+			console.log('populated');
+			populateStorage();
+		} else {
+			console.log('storage retrieved');
+			getStorage();
+			displayLibrary();
+		}
+	}
+};
 
-    let read = document.createElement("button");
-    read.id = "read";  
+storage();
 
-    let remove = document.createElement("button"); 
-    remove.id = "remove";  
+// Firestore
 
-    document.getElementById("container").appendChild(grid);  
-    grid.appendChild(author); 
-    grid.appendChild(title); 
-    grid.appendChild(pages); 
-    grid.appendChild(read); 
-    grid.appendChild(remove); 
-    
-    author.innerHTML = "Author: " + (myLibrary[i].author); 
-    title.innerHTML = "Title: " + (myLibrary[i].title); 
-    pages.innerHTML = "Pages: " + (myLibrary[i].pages); 
-    read.innerText = myLibrary[i].read;    
-    remove.innerHTML = '<i class="fa fa-trash fa-2x"></i>';   
-    
-    remove.addEventListener("click", () => {   
-      index = grid.id;
-      myLibrary.splice(index, 1);       
-      displayLibrary();
-    })   
+const db = firebase.firestore();
 
-    read.addEventListener("click", () => {   
-      index = grid.id;
+const addBookDb = (book) => {
+	db.collection('books').add(bookToObj(book));
+};
 
-      if (read.innerText == "READ") {   
-          read.innerText = "NOT READ"; 
-          readStatus(index, "NOT READ");  
-          populateStorage();
+const bookToObj = (book) => {
+	return {
+		userId: auth.currentUser.uid,
+		title: book.title,
+		author: book.author,
+		pages: Number(book.pages),
+		read: book.read
+	};
+};
 
-      } else if(read.innerText == "NOT READ"){  
-          read.innerText = "READ"; 
-          readStatus(index, "READ");   
-          populateStorage();
-          
-        }})    
-    };  
-} 
+const docsToBooks = (docs) => {
+	return docs.map((doc) => {
+		console.log(doc.data());
+		return new Book(doc.data().title, doc.data().author, doc.data().pages, doc.data().read);
+	});
+};
 
-document.addEventListener("DOMContentLoaded", () => { 
-  if(!localStorage.getItem('library')) {
-    populateStorage();
-  } else {
-    getStorage(); 
-    displayLibrary()
-  }
-})
+const getDocId = async (title) => {
+	const data = await db.collection('books').where('title', '==', title).get();
+	const id = data.docs.map((doc) => doc.id).join('');
+	return id;
+};
+
+const deleteBookDb = async (title) => {
+	console.log(await getDocId(title));
+	db.collection('books').doc(await getDocId(title)).delete();
+};
+
+const updateReadDb = async (title) => {
+	db.collection('books').doc(await getDocId(title)).update({ read: 'NOT READ' });
+};
